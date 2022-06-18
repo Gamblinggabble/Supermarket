e7785e7564ca5adc3ee6c29668798f31b5a127fd
Gabriela Dimcheva <gabriela.dim_cheva@abv.bg>
2022-06-18 10:51:19 +0300

    Format qr code

--- 02.Shop/src/com/company/helperClasses/ReceiptsFileSaver.java
+++ 02.Shop/src/com/company/helperClasses/ReceiptsFileSaver.java
@@ -132,7 +132,8 @@ public class ReceiptsFileSaver implements ReceiptsSaver {
     }
 
     private static String getFormattedReceiptHtml(Receipt receipt) {
-        final String fill = new String(new char[40]).replace("\0", "-") + "<br>";
+        final int fullLength = 40;
+        final String fill = new String(new char[fullLength]).replace("\0", "-") + "<br>";
         StringBuilder sb = new StringBuilder();
         // Касовата бележка трябва да съдържат минимум следната информация: пореден номер,
         // касиер, който издава касовата бележка, дата и час на издаване на касовата бележка, списък със
@@ -181,7 +182,7 @@ public class ReceiptsFileSaver implements ReceiptsSaver {
         sb.append("<br>");
 
         // QR code
-        sb.append("<img " + "src=" + "\"https://barcode.tec-it.com/barcode.ashx?code=MobileQRCode&data=" + getFormattedReceiptTxt(receipt, "*") + "\" >");
+        sb.append("<img style=\"width:110px;height:110px;\"" + "src=" + "\"https://barcode.tec-it.com/barcode.ashx?code=MobileQRCode&data=" + getFormattedReceiptQR(receipt, "*") + "\" >");
         sb.append("<br><br>");
 
         // continued footer
@@ -189,6 +190,70 @@ public class ReceiptsFileSaver implements ReceiptsSaver {
 
         sb.append("</p>");
 
+        return sb.toString();
+    }
+
+    private static String getFormattedReceiptQR(Receipt receipt, String divider) {
+        final int fullLength = 45;
+        final String fill = new String(new char[fullLength]).replace("\0", divider) + "\n";
+        StringBuilder sb = new StringBuilder();
+        // Касовата бележка трябва да съдържат минимум следната информация: пореден номер,
+        // касиер, който издава касовата бележка, дата и час на издаване на касовата бележка, списък със
+        // стоки, които се включват в касовата бележка включително цената и количеството им и общата
+        // стойност, която трябва да се заплати от клиента.
+
+        // header
+        sb.append("\n\n");
+        sb.append(new String(new char[(fullLength - "RECEIPT FROM".length()) / 2]).replace("\0", " "));
+        sb.append("RECEIPT FROM");
+        sb.append(new String(new char[(fullLength - "RECEIPT FROM".length()) / 2]).replace("\0", " "));
+        sb.append("\n");
+        int nameLength = receipt.getSupermarket().getName().length();
+        sb.append(new String(new char[(fullLength - nameLength) / 2]).replace("\0", " "));
+        sb.append(receipt.getSupermarket().getName().toUpperCase(Locale.ROOT));
+        sb.append(new String(new char[(fullLength - nameLength) / 2]).replace("\0", " "));
+        sb.append("\n\n");
+        sb.append(fill);
+
+        // meta data
+        String idMetadata = "Receipt Nr. " + receipt.getId();
+        sb.append(new String(new char[(fullLength - idMetadata.length()) / 2]).replace("\0", " "));
+        sb.append(idMetadata);
+        sb.append(new String(new char[(fullLength - idMetadata.length()) / 2]).replace("\0", " "));
+        sb.append("\n");
+        String date = String.format("%10s%02d/%02d/%4d  %02d:%02d:%02d%10s", " ", receipt.getDate().getDayOfMonth(), receipt.getDate().getMonthValue(), receipt.getDate().getYear()
+                , receipt.getDate().getHour(), receipt.getDate().getMinute(), receipt.getDate().getSecond(), " ");
+        sb.append(date);
+        sb.append("\n");
+        sb.append(fill);
+        sb.append("\n");
+
+        //TODO could eventually add "ITEM     QTY PRC"
+
+        // products
+        for (var product : receipt.getProductList().keySet()) {
+            sb.append(String.format("%-20s %10dx %7.2f", product.getName(), receipt.getProductList().get(product).getFirst(), receipt.getProductList().get(product).getSecond()));
+            sb.append("\n");
+        }
+        sb.append("\n");
+        sb.append(String.format("TOTAL:  %32.2f", receipt.getTotalSum()));
+        sb.append("\n");
+        sb.append(fill);
+        // TODO could eventually add "CASH", "CHANGE"
+
+        // footer
+        String servedBy = "You have been served by " + receipt.getCashier().getName().split(" ")[0];
+        int servedByLength = (servedBy).length();
+        sb.append(new String(new char[(fullLength - servedByLength) / 2]).replace("\0", " "));
+        sb.append(servedBy);
+        sb.append(new String(new char[(fullLength - servedByLength) / 2]).replace("\0", " "));
+        sb.append("\n");
+        sb.append(fill);
+        sb.append("\n");
+        sb.append(new String(new char[15]).replace("\0", " "));
+        sb.append("THANK YOU!");
+        sb.append(new String(new char[15]).replace("\0", " "));
+        sb.append("\n\n");
 
         return sb.toString();
     }
